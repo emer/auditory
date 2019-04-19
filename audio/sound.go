b43478592b1c5f3990ae198743a358a011dea357
rohrlich <john.rohrlich@colorado.edu>
2019-04-19 10:48:50 -0600

    Fill in a few funcs

--- audio/sound.go
+++ audio/sound.go
@@ -4,19 +4,12 @@
 package audio
 
 import (
+	"errors"
 	"fmt"
 	"github.com/go-audio/wav"
 	"os"
+	"time"
 )
-
-//#include <taFiler>
-//#include <float_Matrix>
-//#include <QAudioFormat>
-//#include <QAudioDeviceInfo>
-//#include <taSound_QObj>
-//
-//#ifdef TA_SNDFILE
-//#include <sndfile.hh>
 
 //type Endian int32
 //
@@ -45,17 +38,44 @@ func (snd *Sound) LoadSound(filename string) error {
 		fmt.Printf("couldn't open %s %v", filename, err)
 		return err
 	}
-
 	snd.Decoder = wav.NewDecoder(inFile)
+
+	if snd.Decoder.IsValidFile() != true {
+		err := errors.New("Sound.LoadSound: Invalid wav file")
+		return err
+	}
+	fmt.Printf("sample rate: %v\n", snd.Decoder.SampleRate)
+	duration, err := snd.Decoder.Duration()
+	fmt.Printf("duration: %v\n", duration)
 	defer inFile.Close()
 
 	return err
 }
 
-func (snd *Sound) SampleRate() uint32 {
-	return snd.Decoder.SampleRate
+func (snd *Sound) SampleRate() (uint32, error) {
+	if snd == nil {
+		err := errors.New("Sound.SampleRate: Sound is nil")
+		return 0, err
+	}
+	return snd.Decoder.SampleRate, nil
 }
 
-func (snd *Sound) Channels() uint16 {
-	return snd.Decoder.NumChans
+func (snd *Sound) Channels() (uint16, error) {
+	if snd == nil {
+		err := errors.New("Sound.Channels: Sound is nil")
+		return 0, err
+	}
+	return snd.Decoder.NumChans, nil
+}
+
+func (snd *Sound) Duration() (time.Duration, error) {
+	if snd == nil {
+		err := errors.New("Sound.Duration: Sound is nil")
+		return -1, err
+	}
+	d, err := snd.Decoder.Duration()
+	if err != nil {
+		return d, err
+	}
+	return d, nil
 }
