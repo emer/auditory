96f36ebd10d11d05097f7072bc0bb4467fada93a
rohrlich <john.rohrlich@colorado.edu>
2019-09-27 11:26:13 -0600

    Swap gabor inner/outer x/y's

--- agabor/gabor.go
+++ agabor/gabor.go
@@ -183,18 +183,20 @@ func Conv(ch int, spec Gabor, input input.Input, raw *etensor.Float32, filters i
 	//fMax := ap.Mel.MelFBank.NFilters - spec.SizeFreq
 	fMax := filters - spec.SizeFreq
 
+	maxTimeIdx := raw.Dim(2) // dim 0 is channel
 	tIdx := 0
 	for s := tMin; s < tMax; s, tIdx = s+spec.SpaceTime, tIdx+1 {
 		inSt := s - tOff
-		if tIdx > raw.Dim(4) {
-			fmt.Printf("GaborFilter: time index %v out of range: %v", tIdx, raw.Dim(3))
+		if tIdx > maxTimeIdx {
+			fmt.Printf("GaborFilter: time index %v out of range: %v", tIdx, maxTimeIdx)
 			break
 		}
 
+		maxFreqIdx := raw.Dim(1) // dim 0 is channel
 		fIdx := 0
 		for flt := fMin; flt < fMax; flt, fIdx = flt+spec.SpaceFreq, fIdx+1 {
-			if fIdx > raw.Dim(3) {
-				fmt.Printf("GaborFilter: freq index %v out of range: %v", tIdx, raw.Dim(2))
+			if fIdx > maxFreqIdx {
+				fmt.Printf("GaborFilter: freq index %v out of range: %v", tIdx, maxFreqIdx)
 				break
 			}
 			nf := spec.NFilters
@@ -210,11 +212,11 @@ func Conv(ch int, spec Gabor, input input.Input, raw *etensor.Float32, filters i
 				pos := fSum >= 0.0
 				act := spec.Gain * math32.Abs(fSum)
 				if pos {
-					raw.SetFloat([]int{ch, fi, 0, tIdx, fIdx}, float64(act))
-					raw.SetFloat([]int{ch, fi, 1, tIdx, fIdx}, 0)
+					raw.SetFloat([]int{ch, fIdx, tIdx, 0, fi}, float64(act))
+					raw.SetFloat([]int{ch, fIdx, tIdx, 0, fi}, 0)
 				} else {
-					raw.SetFloat([]int{ch, fi, 0, tIdx, fIdx}, 0)
-					raw.SetFloat([]int{ch, fi, 1, tIdx, fIdx}, float64(act))
+					raw.SetFloat([]int{ch, fIdx, tIdx, 0, fi}, 0)
+					raw.SetFloat([]int{ch, fIdx, tIdx, 0, fi}, float64(act))
 				}
 			}
 		}
