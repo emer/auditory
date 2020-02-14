bb859bef5319e83308fa6a8e8eb583e2532a644c
rohrlich <john.rohrlich@colorado.edu>
2020-02-14 11:52:16 -0800

    - Add doc.go at top level. go.mod needs to be added as well.

--- doc.go
+++ doc.go
@@ -1,5 +1,8 @@
-# auditory
+// Copyright (c) 2019, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
+/*
 Package auditory is the overall repository for audition processing code in Go (golang) focused on filtering speech wav files via mel filters. A further step using gabors provides filtering for input to neural networks. The processing code is split into 4 packages, sound, mel, dft and agabor, that can be used independently. A fifth package, trm, is a work in progress port of Gnuspeech. Example code is in examples/processspeech.
 
 The `sound` package contains code for loading a wav file into a buffer and then converting to a floating point tensor. There are functions for trimming and padding.
@@ -9,3 +12,6 @@ The 'dft' package does a fourier transform and computes the power spectrum on th
 The 'mel' package creates a set of mel filter banks and applies them to the power data to create a spectrogram.
 
 The 'agabor' package produces an edge detector that detects oriented contrast transitions between light and dark which can be convolved with the output of the mel processing.
+*/
+
+package auditory
