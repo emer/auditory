47f2848d8d9f45f50a21fcfce5b11fce8f1248bb
rohrlich <john.rohrlich@colorado.edu>
2020-03-13 20:22:34 -0700

    Add signal plot

--- examples/synth/synth.go
+++ examples/synth/synth.go
@@ -5,11 +5,17 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/emer/auditory/trm"
+	"github.com/emer/etable/eplot"
+	"github.com/emer/etable/etable"
+	"github.com/emer/etable/etensor"
 	_ "github.com/emer/etable/etview" // include to get gui views
 	"github.com/goki/gi/gi"
 	"github.com/goki/gi/gimain"
 	"github.com/goki/gi/giv"
+	"github.com/goki/gi/mat32"
 )
 
 // this is the stub main for gogi that calls our actual
@@ -22,10 +28,39 @@ func main() {
 
 // Synth encapsulates
 type Synth struct {
-	vc trm.VocalTract
+	vt         trm.VocalTract
+	ToolBar    *gi.ToolBar   `view:"-" desc:"the master toolbar"`
+	SignalData *etable.Table `view:"no-inline" desc:"waveform data"`
+	WavePlot   *eplot.Plot2D `view:"-" desc:"waveform plot"`
 }
 
 func (syn *Synth) Defaults() {
+	syn.SignalData = &etable.Table{}
+	syn.ConfigSignalData(syn.SignalData)
+}
+
+// ConfigSignalData
+func (syn *Synth) ConfigSignalData(dt *etable.Table) {
+	dt.SetMetaData("name", "Wave")
+	dt.SetMetaData("desc", "Waveform values -1 to 1")
+	dt.SetMetaData("read-only", "true")
+	dt.SetMetaData("precision", strconv.Itoa(4))
+
+	sch := etable.Schema{
+		{"Amplitude", etensor.FLOAT64, nil, nil},
+	}
+	dt.SetFromSchema(sch, 0)
+}
+
+func (syn *Synth) ConfigWavePlot(plt *eplot.Plot2D, dt *etable.Table) *eplot.Plot2D {
+	plt.Params.Title = "Waveform plot"
+	plt.Params.XAxisCol = "Time"
+	plt.SetTable(dt)
+
+	// order of params: on, fixMin, min, fixMax, max
+	plt.SetColParams("Run", eplot.Off, eplot.FixMin, 0, eplot.FloatMax, 0)
+
+	return plt
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -48,15 +83,19 @@ func (syn *Synth) ConfigGui() *gi.Window {
 
 	tbar := gi.AddNewToolBar(mfr, "tbar")
 	tbar.SetStretchMaxWidth()
-	// vi.ToolBar = tbar
+	syn.ToolBar = tbar
 
 	split := gi.AddNewSplitView(mfr, "split")
-	split.Dim = gi.X
-	split.SetStretchMaxWidth()
-	split.SetStretchMaxHeight()
+	split.Dim = mat32.X
+	split.SetStretchMax()
 
 	sv := giv.AddNewStructView(split, "sv")
 	sv.SetStruct(syn)
+
+	tview := gi.AddNewTabView(split, "tv")
+
+	plt := tview.AddNewTab(eplot.KiT_Plot2D, "wave").(*eplot.Plot2D)
+	syn.WavePlot = syn.ConfigWavePlot(plt, syn.SignalData)
 
 	split.SetSplits(1)
 
@@ -81,9 +120,9 @@ var TheSyn Synth
 
 func mainrun() {
 	TheSyn.Defaults()
-	TheSyn.vc.Init()
-	TheSyn.vc.LoadEnglishPhones()
-	TheSyn.vc.SynthPhones("ee", true, true)
+	TheSyn.vt.Init()
+	TheSyn.vt.LoadEnglishPhones()
+	TheSyn.vt.SynthPhones("ee", true, true)
 
 	win := TheSyn.ConfigGui()
 	win.StartEventLoop()
