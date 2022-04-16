1da2bdf149df254ae4411dfb2c9764f812842fd5
rohrlich <john.rohrlich@gmail.com>
2022-04-15 20:23:33 -0700

    Add field Silence (bool) to Unit struct; Add field Start to Sequence Struct; Rename TimeStop to Stop

--- speech/speech.go
+++ speech/speech.go
@@ -19,22 +19,24 @@ var UnitProps = ki.Props{}
 
 // Unit
 type Unit struct {
-	Name   string  `desc:"the CV (e.g. -- da, go, ku ...), or phones (g, ah, ix ...)"`
-	Start  float64 `desc:"start time of this unit in a particular sequence in milliseconds"`
-	End    float64 `desc:"end time of this unit in a particular sequence in milliseconds"`
-	AStart float64 `desc:"start time of this unit in a particular sequence in milliseconds, adjusted for random start silence and any offset in audio"`
-	AEnd   float64 `desc:"end time of this unit in a particular sequence in milliseconds, adjusted for random start silence and any offset in audio"`
-	Type   string  `desc:"optional info - type of unit, phone, phoneme, word, CV (consonsant-vowel), etc"`
+	Name    string  `desc:"the CV (e.g. -- da, go, ku ...), or phones (g, ah, ix ...)"`
+	Start   float64 `desc:"start time of this unit in milliseconds"`
+	End     float64 `desc:"end time of this unit in milliseconds"`
+	AStart  float64 `desc:"start time of this unit in milliseconds, adjusted for random start silence and any offset in audio"`
+	AEnd    float64 `desc:"end time of this unit in milliseconds, adjusted for random start silence and any offset in audio"`
+	Silence bool    `desc:"is silence, i.e. was desginated as a period of silence"`
+	Type    string  `desc:"optional info - type of unit, phone, phoneme, word, CV (consonsant-vowel), etc"`
 }
 
 // Sequence a sequence of speech units, for example a sequence of phones or words
 type Sequence struct {
-	File     string  `desc:""`
-	ID       string  `desc:"an id to use if the corpus has subsets"`
+	File     string  `desc:"full file path"`
+	ID       string  `desc:"an id to use if the corpus has subsets or for any other purpose"`
 	Sequence string  `desc:"the full sequence of CVs, Phones, Words or whatever the unit"`
 	Text     string  `desc:"the full readable transcription"`
 	Units    []Unit  `desc:"the units of the sequence"`
 	Silence  float64 `desc:"milliseconds of silence added at start of sequence to add variability"`
 	TimeCur  float64 `desc:"current time in milliseconds since start of sequence"`
-	TimeStop float64 `desc:"start of final silence in milliseconds"`
+	Start    float64 `desc:"start of sound in milliseconds, many files have initial silence"`
+	Stop     float64 `desc:"start of final silence in milliseconds"`
 }
